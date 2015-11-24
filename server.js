6a60aa6639e207b1c3e23e2402375b16b9e94bed
papapin <borabora1212>
2015-11-24 16:24:54 +0200

    finish

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// MEAN Stack RESTful API Tutorial - Contact List App
 
 var express = require('express');
 var app = express();
@@ -19,10 +20,38 @@ app.get('/contactlist', function (req, res) {
 
 app.post('/contactlist', function (req, res) {
   console.log(req.body);
-    db.contactlist.insert(req.body, function(err, doc) {
+  db.contactlist.insert(req.body, function(err, doc) {
     res.json(doc);
   });
-  
+});
+
+app.delete('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/contactlist/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.contactlist.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
 });
 
 app.listen(3000);
+console.log("Server running on port 3000");
